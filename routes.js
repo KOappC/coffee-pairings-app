6f5d9a25ddf71dffb06502f0fe65efa21dcb5707
Kyle Apsey <kyleapsey@Kyles-MacBook-Pro.local>
2017-12-12 15:32:40 -0500

    cleaned up code a bit in the backend

--- routes.js
+++ routes.js
@@ -3,25 +3,7 @@ var routes = express.Router();
 var pool = require("./pg-connection-pool.js");
 
 
-/*routes.get("/flavors", function(req, res) {
-    res.send("GET done");
-    console.log("GET has completed successfully");
-});
 
-routes.post("/flavors", function(req, res) {
-    res.send("POST done");
-    console.log("POST has completed successfully");
-});
-
-routes.put("/flavors/:id", function(req, res) {
-    res.send("PUT done");
-    console.log("PUT has completed successfully");
-});
-
-routes.delete("/flavors/:id", function(req,res) {
-    res.send("DELETE done, oh no!");
-    console.log("DELETE has completed successfully");
-});*/
 routes.get("/flavors", function(req, res) {
     pool.query("select DISTINCT bean from tasting").then(function(result) {
         res.send(result.rows);
